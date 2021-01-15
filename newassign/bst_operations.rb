91167237f73bdbb7b7896b95026a18408ecd1ff1
deshpandeaniruddh5 <58878680+deshpandeaniruddh5@users.noreply.github.com>
2021-01-15 15:35:42 +0530

    Add files via upload

--- newassign/bst_operations.rb
+++ newassign/bst_operations.rb
@@ -1,182 +1,52 @@
 require 'io/console'
-class Node 
-  attr_accessor :value, :left, :right
-  
-  def initialize (value)
-    @value = value 
-    @left = nil 
-    @right = nil 
-  end
-end 
-
-class BST    
-  def insertElement(root,key)
-    if root==nil
-      return Node.new(key) 
-    end  
-    if key < root.value
-      root.left=insertElement(root.left,key)
-    else
-      root.right=insertElement(root.right,key)
-    end
-    return root
-  end
-
-  def smallestElement(root)
-    if root==nil 
-      return nil
-    elsif(root.left==nil)
-      return root.value
-    else
-      return smallestElement(root.left)
-    end   
-  end
-
-  def largestElement(root)
-    if root==nil 
-      return nil
-    elsif(root.right==nil)
-      return root.value
-    else
-      return largestElement(root.right)
-    end   
-  end
-
-  def printInorder(root)
-    if root==nil
-      return
-    end
-    printInorder(root.left)
-    puts root.value
-    printInorder(root.right)
-  end
-
-  def printPreorder(root,arr)
-    if root==nil
-      return
-    end
-    arr.push(root.value)
-    puts root.value
-    printPreorder(root.left,arr)
-    printPreorder(root.right,arr)
-  end
-
-  def printPostorder(root)
-    if root==nil
-      return
-    end
-    printPostorder(root.left)
-    printPostorder(root.right)
-    puts root.value
-  end  
-
-  def searchElement(root,key)
-    if root==nil
-      return false
-    end
-    if key < root.value 
-      return searchElement(root.left,key)
-    elsif key > root.value 
-      return searchElement(root.right,key)
-    else 
-      return true
-    end    
-  end  
-
-  def printAllPaths(root,arr)
-    arr.push(root.value.to_i)
-    if root.left != nil 
-      printAllPaths(root.left,arr)
-    end  
-    if root.right != nil
-      printAllPaths(root.right,arr)
-    end
-    if (root.left==nil && root.right==nil)
-      puts arr
-    end
-    arr.pop()     
-  end
-
-  def minValueNode(node)
-    current = node
-    while(current.left!=nil)
-      current = current.left
-    end
-    return current
-  end  
-
-  def deleteNode(root, key)
-    if root == nil
-      return root
-    end    
-    if(key<root.value)
-      root.left = deleteNode(root.left,key)
-    elsif (key>root.value)
-      root.right = deleteNode(root.right,key)
-    else
-      if root.left == nil
-        temp = root.right
-        root = nil
-        return temp
-      elsif root.right == nil
-        temp = root.left
-        root = nil
-        return temp
-      end
-      temp = minValueNode(root.right)
-      root.value = temp.value
-      root.right = deleteNode(root.right, temp.value)
-    end
-    return root
-  end  
-end  
-
-pre=[]
-File.foreach(ARGV[0]) {|line| pre.push(line.to_i)}
-p pre
+require_relative 'class_BST'
+require_relative 'class_node'
+preorder=[]
+File.foreach(ARGV[0]) { |line| preorder.push(line.to_i) }
+p 'Preorder array from the loaded file:'
+p preorder
 bst=BST.new
 root=nil
-
-for i in pre
-  root=bst.insertElement(root,i)
+for element in preorder
+  root=bst.insert_element(root,element)
 end
-
 while 1
   command=$stdin.gets
   if(command.chomp=="quit")
     ftwo = File.open(ARGV[0], "w") 
     ftwo.truncate(0)
     preorder=[]
-    bst.printPreorder(root,preorder)
+    bst.print_preorder(root,preorder)
     for x in preorder  
       ftwo.puts(x) 
     end
     exit()
   elsif command.chomp=="insert"
+    puts 'Enter number to insert:'
     key=$stdin.gets
-    root=bst.insertElement(root,key.chomp.to_i)
+    root=bst.insert_element(root,key.chomp.to_i)
   elsif command.chomp=="largest"
-    puts bst.largestElement(root)
+    puts bst.largest_element(root)
   elsif command.chomp=="smallest"
-    puts bst.smallestElement(root)
+    puts bst.smallest_element(root)
   elsif command.chomp=="inorder"
-    bst.printInorder(root)
+    bst.print_inorder(root)
   elsif command.chomp=="preorder"
-    bst.printPreorder(root,[]) 
+    bst.print_preorder(root,[]) 
   elsif command.chomp=="postorder"
-    bst.printPostorder(root)
+    bst.print_postorder(root)
   elsif command.chomp=="search"
+    puts 'Enter number to search:'
     key=$stdin.gets
-    p bst.searchElement(root,key.chomp.to_i)
+    p bst.search_element(root,key.chomp.to_i)
   elsif command.chomp=="printallpaths"
     arr=[]
-    bst.printAllPaths(root,arr)
+    bst.print_allPaths(root,arr)
   elsif command.chomp=="delete"
+    puts 'Enter number to be deleted:'
     key=$stdin.gets
-    root=bst.deleteNode(root,key.chomp.to_i)  
+    root=bst.delete_node(root,key.chomp.to_i)  
   else 
     p "Invalid Command"
   end
-end     
-
-
+end    
